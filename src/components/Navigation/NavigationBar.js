b6bd6aa905d49758bf1a3091857da17c177f4b6c
Jacob Radcliffe <jradcli2@my.westga.edu>
2018-07-21 09:27:49 -0500

    Navbar complete: Brought in React Icons libary for nav bar.

--- src/components/Navigation/NavigationBar.js
+++ src/components/Navigation/NavigationBar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { FaHome, FaGithub, FaLinkedin } from 'react-icons/lib/fa';
+
 import "./NavigationBar.css";
 
 const NavigationBar = () => {
     return (
         <nav className="navbar navbar navbar-expand-sm navbar-dark bg-dark">
             <Link to="/" className="nav-link">
-                <ion-icon name="home" />
+                <FaHome className="navbar__link__icon" />
             </Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
@@ -17,7 +19,7 @@ const NavigationBar = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button">Dropdown</a>
+                        <a className="nav-link dropdown-toggle" data-toggle="dropdown" role="button">Dropdown</a>
                         <div className="dropdown-menu">
                             <Link to="/htmlRefVsReactRef" className="dropdown-item">Demo1</Link>
                             <Link to="/htmlRefVsReactRef" className="dropdown-item">Demo2</Link>
@@ -33,11 +35,11 @@ const NavigationBar = () => {
                 </ul>
 
                 <div>
-                    <a href="https://github.com/jeradcliffe/" target="_blank" className="nav-item">
-                        <ion-icon name="code" />
+                    <a href="https://github.com/jeradcliffe/" target="_blank" rel="noopener noreferrer" className="nav-item">
+                        <FaGithub className="navbar__link__icon" />
                     </a>
-                    <a href="https://www.linkedin.com/in/jacob-radcliffe-36a20579" target="_blank" className="nav-item">
-                        <ion-icon name="logo-linkedin" />
+                    <a href="https://www.linkedin.com/in/jacob-radcliffe-36a20579" rel="noopener noreferrer" target="_blank" className="nav-item">
+                        <FaLinkedin className="navbar__link__icon" />
                     </a>
                 </div>
             </div>
